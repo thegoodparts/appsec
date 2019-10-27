08f556a4a52494373c771d1c88acdc20c6d96f4d
Eduardo Eisman <eeisman@theworkshop.com>
2019-10-27 20:54:27 +0100

    AppSec > Validation > Redirect

--- appsec-validation/appsec-validation-redirect/src/main/java/internal/appsec/validation/redirect/controller/AuthController.java
+++ appsec-validation/appsec-validation-redirect/src/main/java/internal/appsec/validation/redirect/controller/AuthController.java
@@ -6,10 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
-@Slf4j
 class AuthController {
 
     // TODO Get whitelisted referers from application resources
